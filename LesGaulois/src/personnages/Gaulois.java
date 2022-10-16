eb09f78b1e54f5c6dd074447998b96c0dec26413
mango <mango@YnnadION>
2022-10-16 12:46:43 +0200

    First commit

---
+++ LesGaulois/src/personnages/Gaulois.java
@@ -0,0 +1,51 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion;
+
+	public Gaulois(String nom, int force, int effetPotion) {
+		super();
+		this.nom = nom;
+		this.force = force;
+		this.effetPotion = effetPotion;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+	
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : "; 
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom() );
+		romain.recevoirCoup(force/3);
+		
+	}
+	
+
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix", 8, 1);
+		System.out.println(asterix.nom);
+	
+	}
+	
+	
+
+}
+
+
